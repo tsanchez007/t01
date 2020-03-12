cc737dd1f107588034447a749bb9cfd51b53fd8e
tsanchez007 <info.tauliosanchez@gmail.com>
2020-03-11 22:00:37 -0400

    Prueba02 menu Repos
    Prueba02 menu Repos

--- js/main.js
+++ js/main.js
@@ -2,6 +2,29 @@
 (function ($) {
     "use strict";
 
+$(document).ready(main);
+
+var contador = 1;
+
+function main(){
+    $('.menu_bar').click(function(){
+        // $('nav').toggle(); 
+
+        if(contador == 1){
+            $('nav').animate({
+                left: '0'
+            });
+            contador = 0;
+        } else {
+            contador = 1;
+            $('nav').animate({
+                left: '-100%'
+            });
+        }
+
+    });
+
+};
     /*==================================================================
     [ Focus Contact2 ]*/
     $('.input100').each(function(){
